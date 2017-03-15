314d58651df1a3e26528daa182f016e3fe715763
oliver254 <oliver254@hotmail.fr>
2017-03-16 00:00:54 +0100

    Créer les angles minute et seconde

--- src/Reveil.UWP/Controls/CircularClock.cs
+++ src/Reveil.UWP/Controls/CircularClock.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
@@ -22,21 +16,25 @@ namespace Reveil.UWP.Controls
     public sealed class CircularClock : Control
     {
         #region Champs
-        public static readonly DependencyProperty AngleProperty = 
-            DependencyProperty.Register(nameof(Angle), typeof(double), typeof(CircularClock), new PropertyMetadata(120d, new PropertyChangedCallback(OnValueChanged)));
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register(nameof(Duration), typeof(TimeSpan?), typeof(CircularClock), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty MinuteAngleProperty =
+            DependencyProperty.Register(nameof(MinuteAngle), typeof(double), typeof(CircularClock), new PropertyMetadata(0d, OnMinuteAngleChanged));
 
         public static readonly DependencyProperty RadiusProperty = 
-            DependencyProperty.Register(nameof(Radius), typeof(int), typeof(CircularClock), new PropertyMetadata(25, new PropertyChangedCallback(OnValueChanged)));
+            DependencyProperty.Register(nameof(Radius), typeof(int), typeof(CircularClock), new PropertyMetadata(25));
 
         public static readonly DependencyProperty ScaleBrushProperty =
             DependencyProperty.Register(nameof(ScaleBrush), typeof(Brush), typeof(CircularClock), new PropertyMetadata(new SolidColorBrush(Colors.DarkGray)));
+
+        public static readonly DependencyProperty SecondAngleProperty =
+            DependencyProperty.Register(nameof(SecondAngle), typeof(double), typeof(CircularClock), new PropertyMetadata(0d, OnSecondAngleChanged));
 
 
         public static readonly DependencyProperty StrokeThicknessProperty = 
             DependencyProperty.Register("StrokeThickness", typeof(int), typeof(CircularClock), new PropertyMetadata(5));
 
-        public static readonly DependencyProperty TimeSpanProperty =
-            DependencyProperty.Register(nameof(TimeSpan), typeof(TimeSpan), typeof(CircularClock), new PropertyMetadata(TimeSpan.Zero, new PropertyChangedCallback(OnValueChanged)));
 
         public static readonly DependencyProperty TrailBrushProperty =
             DependencyProperty.Register(nameof(TrailBrush), typeof(Brush), typeof(CircularClock), new PropertyMetadata(new SolidColorBrush(Colors.DarkBlue)));
@@ -44,44 +42,70 @@ namespace Reveil.UWP.Controls
         private const string ContainerPartName = "PART_Container";
         private const string ScalePartName = "PART_Scale";
         private const string TrailPartName = "PART_Trail";
+
+        private DateTime? _end;
+        private DispatcherTimer _timer;
         #endregion
 
         #region Constructeurs
         public CircularClock()
         {
-            this.DefaultStyleKey = typeof(CircularClock);
+            DefaultStyleKey = typeof(CircularClock);
+            _timer = new DispatcherTimer();
+            _timer.Tick += Timer_Tick;
         }
+
+
         #endregion
 
         #region Propriétés
-        public double Angle
+        /// <summary>
+        /// Obtient ou définit la durée
+        /// </summary>
+        public TimeSpan? Duration
         {
-            get { return (double)GetValue(AngleProperty); }
-            set { SetValue(AngleProperty, value); }
+            get { return (TimeSpan?)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
         }
 
+        /// <summary>
+        /// Obtient ou définit l'angle minute
+        /// </summary>
+        public double MinuteAngle
+        {
+            get { return (double)GetValue(MinuteAngleProperty); }
+            set { SetValue(MinuteAngleProperty, value); }
+        }
+
+        /// <summary>
+        /// Obtient ou définit le rayon
+        /// </summary>
         public int Radius
         {
             get { return (int)GetValue(RadiusProperty); }
             set { SetValue(RadiusProperty, value); }
         }
 
+
         public Brush ScaleBrush
         {
             get { return (Brush)GetValue(ScaleBrushProperty); }
             set { SetValue(ScaleBrushProperty, value); }
-        }          
+        }
+        
+        /// <summary>
+        /// Obtient ou définit l'angle seconde
+        /// </summary>
+        public double SecondAngle
+        {
+            get { return (double)GetValue(SecondAngleProperty); }            
+            set { SetValue(SecondAngleProperty, value); }
+        }
 
         public int StrokeThickness
         {
             get { return (int)GetValue(StrokeThicknessProperty); }
             set { SetValue(StrokeThicknessProperty, value); }
-        }
-
-        public TimeSpan TimeSpan
-        {
-            get { return (TimeSpan)GetValue(TimeSpanProperty); }
-            set { SetValue(TimeSpanProperty, value); }
         }
 
         public Brush TrailBrush
@@ -92,9 +116,37 @@ namespace Reveil.UWP.Controls
         #endregion
 
         #region Méthodes
+        /// <summary>
+        /// Lance la montre
+        /// </summary>
+        public void Start()
+        {
+            _timer.Stop();
+            if(Duration != null)
+            {
+                _end = DateTime.Now + Duration;
+
+            }
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Arrête la montre
+        /// </summary>
+        public void Stop()
+        {            
+            _timer.Stop();
+            _end = null;
+        }
+
         protected override void OnApplyTemplate()
         {
-            OnValueChanged(this);
+            if (GetTemplateChild(ScalePartName) is Path scalePath)
+            {
+                scalePath.Stroke = ScaleBrush;
+                scalePath.StrokeThickness = StrokeThickness;
+                RenderArc(360, scalePath);
+            }
             base.OnApplyTemplate();
 
         }
@@ -109,30 +161,31 @@ namespace Reveil.UWP.Controls
             return new Point(x, y);
         }
 
-        private static void OnValueChanged(DependencyObject sender)
+        private static double ConvertTimeToAngle(int time)
+        {
+            double dtime = Convert.ToDouble(time);
+            return (dtime * 6);
+        }
+
+
+        private static void OnMinuteAngleChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            // [TODO]
+        }
+
+        private static void OnSecondAngleChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
         {
             CircularClock circle = (CircularClock)sender;
 
-            Path scalePath = circle.GetTemplateChild(ScalePartName) as Path;
-            if(scalePath != null)
-            {
-                scalePath.Stroke = circle.ScaleBrush;
-                scalePath.StrokeThickness = circle.StrokeThickness;
-                circle.RenderArc(360, scalePath);                
-            }
-            Path trailPath = circle.GetTemplateChild(TrailPartName) as Path;
-            if(trailPath != null)
+            // trainée des secondes
+            if (circle.GetTemplateChild(TrailPartName) is Path trailPath)
             {
                 trailPath.Stroke = circle.TrailBrush;
-                trailPath.StrokeThickness = circle.StrokeThickness;                
-                circle.RenderArc(circle.Angle, trailPath);
+                trailPath.StrokeThickness = circle.StrokeThickness;
+                circle.RenderArc(circle.SecondAngle, trailPath);
             }
         }
 
-        private static void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
-        {
-            OnValueChanged(sender);
-        }
 
         private void RenderArc(double angle, Path pathRoot)
         {
@@ -168,6 +221,30 @@ namespace Reveil.UWP.Controls
             arcSegment.IsLargeArc = largeArc;
 
             pathRoot.Data = pg;
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            int seconds = 0;
+            int minutes = 0;
+            if(_end != null)
+            {
+                var ts =  (_end.Value - DateTime.Now);
+                seconds = ts.Seconds;
+                minutes = ts.Minutes;
+            }
+            else
+            {
+                seconds = DateTime.Now.Second;
+                minutes = DateTime.Now.Minute;
+            }
+            SecondAngle = ConvertTimeToAngle(seconds);
+            MinuteAngle = ConvertTimeToAngle(minutes);
         }
         #endregion
     }
